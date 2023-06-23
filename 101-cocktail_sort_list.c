8377ddf28e994e57767ffa2e14de2c573bcfb2c5
Dr-dyrane <halodyrane@gmail.com>
2023-06-23 14:12:23 -0700

    dyrane is awesome

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -23,7 +23,7 @@ void cocktail_sort_list(listint_t **list)
 		{
 			if (current->n > current->next->n)
 			{
-				swap_nodes(list, current, current->next);
+				swap_node(list, current, current->next);
 				print_list(*list);
 				swapped = 1;
 			}
@@ -48,32 +48,4 @@ void cocktail_sort_list(listint_t **list)
 				current = current->prev;
 		}
 	}
-}
-
-/**
- * swap_node - Swaps a node with its previous one in a doubly linked list
- *
- * @node: Node to be swapped
- * @list: Double pointer to the head of the list
- *
- * Return: Pointer to the new position of the swapped node
- */
-listint_t *swap_node(listint_t *node, listint_t **list)
-{
-	listint_t *prev_node = node->prev, *current = node;
-
-	prev_node->next = current->next;
-	if (current->next)
-		current->next->prev = prev_node;
-
-	current->next = prev_node;
-	current->prev = prev_node->prev;
-	prev_node->prev = current;
-
-	if (current->prev)
-		current->prev->next = current;
-	else
-	    *list = current;
-
-	return (current);
 }
