974584b5aef65dfcb402f71716b66584d353e50b
Dr-dyrane <halodyrane@gmail.com>
2023-06-27 10:53:12 -0700

    dyrane is awesome

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 
 /**
- * swap_nodes - Swaps two nodes in a doubly linked list.
+ *swap_nodes - Swaps two nodes in a doubly linked list.
  *
- * @list: Double pointer to the head of the linked list
- * @node1: First node to swap
- * @node2: Second node to swap
+ *@list: Double pointer to the head of the linked list
+ *@node1: First node to swap
+ *@node2: Second node to swap
  */
 void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2)
 {
@@ -27,18 +27,18 @@ void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2)
 }
 
 /**
- * cocktail_sort_list - Sorts a doubly linked list of integers
+ *cocktail_sort_list - Sorts a doubly linked list of integers
  *                    in ascending order
  *                    using the Cocktail shaker sort algorithm.
  *
- * @list: Double pointer to the head of the linked list
+ *@list: Double pointer to the head of the linked list
  */
 void cocktail_sort_list(listint_t **list)
 {
 	int swapped;
 	listint_t *current;
 
-	if (list == NULL || *list == NULL || (*list)->next == NULL)
+	if (list == '\0' || (*list) == '\0' || (*list)->next == '\0')
 		return;
 
 	do {
@@ -53,7 +53,7 @@ void cocktail_sort_list(listint_t **list)
 			}
 		}
 
-		if (!swapped)
+		if (swapped == 0)
 			break;
 
 		swapped = 0;
@@ -66,5 +66,5 @@ void cocktail_sort_list(listint_t **list)
 				swapped = 1;
 			}
 		}
-	} while (swapped);
+	} while (swapped == 1);
 }
