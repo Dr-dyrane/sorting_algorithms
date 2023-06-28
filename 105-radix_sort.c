58633643056b0b827279a96bdd15a6174e734358
Dr-dyrane <halodyrane@gmail.com>
2023-06-28 01:03:05 -0700

    dyrane is awesome

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -12,21 +12,21 @@
  */
 int get_max(int *array, int size)
 {
-    int max_value = array[0];
-    int i;
+	int max_value = array[0];
+	int i;
 
-    for (i = 1; i < size; i++)
-    {
-        if (array[i] > max_value)
-            max_value = array[i];
-    }
+	for (i = 1; i < size; i++)
+	{
+		if (array[i] > max_value)
+			max_value = array[i];
+	}
 
-    return max_value;
+	return (max_value);
 }
 
 /**
  * counting_sort - Performs counting sort on an array of integers based on the
- *                 specified digit represented by 'exp'.
+ *                specified digit represented by 'exp'.
  * @array: The array to be sorted.
  * @size: The size of the array.
  * @exp: The exponent indicating the digit to consider.
@@ -34,32 +34,34 @@ int get_max(int *array, int size)
  */
 void counting_sort(int *array, size_t size, int exp, int *output)
 {
-    int count[10] = {0};
-    int i;
+	int count[10] = { 0 };
 
-    /* Store count of occurrences in count[] */
-    for (i = 0; i < (int)size; i++)
-        count[(array[i] / exp) % 10]++;
+	int i;
 
-    /*
-     * Calculate cumulative count.
-     * Change count[i] so that count[i] now contains actual
-     * position of this digit in output[]
-     */
-    for (i = 1; i < 10; i++)
-        count[i] += count[i - 1];
+	/* Store count of occurrences in count[] */
+	for (i = 0; i < (int) size; i++)
+		count[(array[i] / exp) % 10]++;
 
-    /* Build the output array */
-    for (i = (int)size - 1; i >= 0; i--)
-    {
-        output[count[(array[i] / exp) % 10] - 1] = array[i];
-        count[(array[i] / exp) % 10]--;
-    }
+	/*
+	 * Calculate cumulative count.
+	 * Change count[i] so that count[i] now contains actual
+	 * position of this digit in output[]
+	 */
+	for (i = 1; i < 10; i++)
+		count[i] += count[i - 1];
 
-    /* Copy the output array to array[], so that array[] now
-     * contains sorted numbers according to current digit
-     */
-    memcpy(array, output, size * sizeof(int));
+	/* Build the output array */
+	for (i = (int) size - 1; i >= 0; i--)
+	{
+		output[count[(array[i] / exp) % 10] - 1] = array[i];
+		count[(array[i] / exp) % 10]--;
+	}
+
+	/*
+	 * Copy the output array to array[], so that array[] now
+	 * contains sorted numbers according to current digit
+	 */
+	memcpy(array, output, size * sizeof(int));
 }
 
 /**
@@ -69,21 +71,22 @@ void counting_sort(int *array, size_t size, int exp, int *output)
  */
 void radix_sort(int *array, size_t size)
 {
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+		return;
 
-    int maximum = get_max(array, size);
-    int *output = malloc(size * sizeof(int));
+	int *output = malloc(size * sizeof(int));
 
-    if (output == NULL)
-        return;
+	if (output == NULL)
+		return;
 
-    int exp;
-    for (exp = 1; maximum / exp > 0; exp *= 10)
-    {
-        counting_sort(array, size, exp, output);
-        print_array(array, size);
-    }
+	int maximum = get_max(array, size);
+	int exp;
 
-    free(output);
+	for (exp = 1; maximum / exp > 0; exp *= 10)
+	{
+		counting_sort(array, size, exp, output);
+		print_array(array, size);
+	}
+
+	free(output);
 }
